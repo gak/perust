47c77e8099624a0af1d7506eb7ccc3f668084d7e
gak <gak@gak0.com>
2020-05-29 07:51:02 +1000

    5

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 pub mod pe_001;
-pub use pe_001::*;
 pub mod pe_002;
-pub use pe_002::*;
 pub mod pe_003;
-pub use pe_003::*;
 pub mod pe_004;
-pub use pe_004::*;
 pub mod pe_005;
-pub use pe_005::*;
--- src/pe_005.rs
+++ src/pe_005.rs
@@ -10,10 +10,22 @@ mod tests {
     #[test]
     fn test() {
         assert_eq!(divider(10), 2520);
-        assert_eq!(divider(20), 9999);
+        assert_eq!(divider(20), 232792560);
     }
 }
 
 pub fn divider(max: u32) -> u32 {
-    todo!()
+    let mut value = max;
+
+    loop {
+        for factor in (1..=max-1).rev() {
+            if factor == 1 {
+                return value
+            }
+            if value % factor != 0 {
+                break
+            }
+        }
+        value += max
+    }
 }
