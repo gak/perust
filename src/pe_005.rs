a4f5a0617ed9a2331744634b090e27a4d611bfdd
gak <gak@gak0.com>
2020-05-29 07:30:54 +1000

    5 wip

---
+++ src/pe_005.rs
@@ -0,0 +1,19 @@
+/**
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        // assert_eq!(divider(1..=10), 2520);
+        // assert_eq!(divider(1..=20), 9999);
+    }
+}
+
+fn divider(range: ()) {
+    todo!();
+}
